654cb4e580c8794e2dc80dc454c393d3037ebf41
Ilia Ilinykh <ilyasyoy@gmail.com>
2022-07-08 15:54:26 +0300

    Lots of useful stuff with LS and git shortcuts

---
+++ .config/nvim/plugin/lsp/lspconfig-installer-settings.lua
@@ -0,0 +1,12 @@
+-- https://github.com/williamboman/nvim-lsp-installer
+
+require("nvim-lsp-installer").setup({
+    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+    ui = {
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    }
+})
