4f6233df0586b1624044f6749e8d592cb0b7df82
Ilia Ilinykh <ilyasyoy@gmail.com>
2022-11-29 18:13:37 +0300

    Some settings and spelling

--- .config/nvim/lua/settings/telekasten.lua
+++ .config/nvim/lua/settings/telekasten.lua
@@ -6,7 +6,7 @@ local wiki_home = vim.fn.expand("~/vimwiki")
 local diary_home = wiki_home .. "/diary"
 local templates_home = wiki_home .. "/meta/templates"
 
-telekasten.setup({
+telekasten.setup{
     home                        = wiki_home,
     take_over_my_home           = true,
     auto_set_filetype           = false,
@@ -26,7 +26,7 @@ telekasten.setup({
     template_new_daily          = templates_home .. "/daily template.md",
     template_new_weekly         = templates_home .. "/weekly template.md",
     image_link_style            = "wiki",
-    sort                        = "filename",
+    sort                        = "modified",
     plug_into_calendar          = true,
     calendar_opts               = {
         weeknm = 1,
@@ -43,27 +43,30 @@ telekasten.setup({
     new_note_location           = "prefer_home",
     rename_update_links         = true,
     media_previewer             = "telescope-media-files",
-})
+}
 
 -- Telekasten commands
 
-local map_normal = require("functions/map").map_normal
+local mapping = require("functions/map")
+local map_n = mapping.map_normal
+local map_v = mapping.map_visual_and_select
 
-map_normal("<leader>z", "<cmd>Telekasten<CR>")
+map_n("<leader>z", "<cmd>Telekasten<CR>")
 
-map_normal("<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
-map_normal("<leader>zT", "<cmd>Telekasten show_tags<CR>")
-map_normal("<leader>zt", "<cmd>Telekasten toggle_todo<CR>")
-map_normal("<leader>zz", "<cmd>Telekasten follow_link<CR>")
-map_normal("<leader>zl", "<cmd>Telekasten insert_link<CR>")
-map_normal("<leader>zn", "<cmd>Telekasten new_note<CR>")
-map_normal("<leader>zN", "<cmd>Telekasten new_templated_note<CR>")
+map_n("<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
+map_n("<leader>zT", "<cmd>Telekasten show_tags<CR>")
+map_n("<leader>zt", "<cmd>Telekasten toggle_todo<CR>")
+map_v("<leader>zt", "<cmd>Telekasten toggle_todo<CR>")
+map_n("<leader>zz", "<cmd>Telekasten follow_link<CR>")
+map_n("<leader>zl", "<cmd>Telekasten insert_link<CR>")
+map_n("<leader>zn", "<cmd>Telekasten new_note<CR>")
+map_n("<leader>zN", "<cmd>Telekasten new_templated_note<CR>")
 
-map_normal("<leader>zd", "<cmd>Telekasten goto_today<CR>")
-map_normal("<leader>zw", "<cmd>Telekasten goto_thisweek<CR>")
-map_normal("<leader>zc", "<cmd>Telekasten show_calendar<CR>")
+map_n("<leader>zd", "<cmd>Telekasten goto_today<CR>")
+map_n("<leader>zw", "<cmd>Telekasten goto_thisweek<CR>")
+map_n("<leader>zc", "<cmd>Telekasten show_calendar<CR>")
 
-map_normal("<leader>zrn", "<cmd>Telekasten rename_note<CR>")
+map_n("<leader>zrn", "<cmd>Telekasten rename_note<CR>")
 
-map_normal("<leader>zff", "<cmd>Telekasten find_notes<CR>")
-map_normal("<leader>zfg", "<cmd>Telekasten search_notes<CR>")
+map_n("<leader>zff", "<cmd>Telekasten find_notes<CR>")
+map_n("<leader>zfg", "<cmd>Telekasten search_notes<CR>")
