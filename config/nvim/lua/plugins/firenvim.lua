dcb6bdaf121b288d53bf77717e309cf9f629ee37
Ilia Ilinykh <ilyasyoy@gmail.com>
2023-02-15 01:44:26 +0300

    firenvim
    settings for easy usage

--- config/nvim/lua/plugins/firenvim.lua
+++ config/nvim/lua/plugins/firenvim.lua
@@ -5,6 +5,27 @@ return {
             vim.fn["firenvim#install"](0)
         end,
 
+        init = function()
+            vim.cmd [[
+                let g:firenvim_config = { 
+                    \ 'globalSettings': {
+                        \ 'alt': 'all',
+                    \  },
+                    \ 'localSettings': {
+                        \ '.*': {
+                            \ 'takeover': 'never',
+                        \ },
+                    \ }
+                \ }
+                if exists('g:started_by_firenvim')
+                    set guifont=Hack:h20
+
+                else
+                    set laststatus=2
+                endif
+            ]]
+        end,
+
         -- Lazy load firenvim
         -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
         cond = not not vim.g.started_by_firenvim,
