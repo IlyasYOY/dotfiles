f947f7d5d298ae942232ba82ea884e4e9909ccc4
Ilia Ilinykh <ilyasyoy@gmail.com>
2023-03-27 16:33:14 +0300

    surround

---
+++ config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,9 @@
+return {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup {
+            move_cursor = false
+        }
+    end,
+}
