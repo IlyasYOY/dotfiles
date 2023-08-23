15bba1166d3dbe8aeaadf7766339cfaa4978a071
Ilia Ilinykh <ilyasyoy@gmail.com>
2023-08-23 23:11:34 +0300

    feat: global status line

--- config/nvim/lua/ilyasyoy/init.lua
+++ config/nvim/lua/ilyasyoy/init.lua
@@ -4,6 +4,9 @@ vim.g.netrw_banner = 0 -- Now we won't have bloated top of the window
 vim.g.netrw_liststyle = 3 -- Now it will be a tree view
 vim.g.netrw_bufsettings = "nu nobl"
 
+vim.opt.laststatus = 3
+vim.cmd "highlight WinSeparator guibg=None"
+
 vim.opt.spelllang = "ru_ru,en_us"
 vim.opt.spellfile = vim.fn.expand "~/.config/nvim/spell/custom.utf-8.add"
 
