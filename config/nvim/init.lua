08b7fcdb504bc17a7ddd9e263e7ed54b24e8caaa
Ilya Ilinykh <Ilyasyoy@gmail.com>
2023-08-24 17:13:41 +0300

    feat: secret settings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,3 +4,7 @@ require "ilyasyoy"
 require "ilyasyoy.global"
 
 require "ilyasyoy.mapping"
+
+-- Here I load files with custom settings for machine.
+-- This lua file is hidden from VCS, so I can do tricky stuff there.
+pcall(require, "ilyasyoy.hidden")
