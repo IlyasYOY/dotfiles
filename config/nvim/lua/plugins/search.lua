fbfdd76c5bcfba596f497a9a91895ff641ef6de9
Ilia Ilinykh <ilyasyoy@gmail.com>
2023-08-28 23:09:43 +0300

    fix: remove fzf

--- config/nvim/lua/plugins/search.lua
+++ config/nvim/lua/plugins/search.lua
@@ -9,16 +9,6 @@ return {
         end,
     },
     {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-        },
-        config = function()
-            require("telescope").load_extension "fzf"
-        end,
-    },
-    {
         "aaronhallaert/advanced-git-search.nvim",
         config = function()
             require("telescope").load_extension "advanced_git_search"
