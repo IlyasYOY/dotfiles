7b2c92e02dc2ca34f44c99e024be56fde554b71d
Ilia Ilinykh <ilyasyoy@gmail.com>
2023-08-28 23:09:20 +0300

    fix: obs.nvim reconfig

--- config/nvim/lua/plugins/obs.lua
+++ config/nvim/lua/plugins/obs.lua
@@ -17,64 +17,16 @@ return {
                 },
             }
 
-            vim.keymap.set("n", "<leader>nn", function()
-                obs.vault:run_if_note(function()
-                    obs.vault:follow_link()
-                end)
-            end, { desc = "navigate to note" })
-
-            vim.keymap.set("n", "<leader>nN", function()
-                local input = vim.fn.input {
-                    prompt = "New note name: ",
-                    default = "",
-                }
-                local file = obs.vault:create_note(input)
-                if file then
-                    file:edit()
-                else
-                    vim.notify("Note '" .. input .. "' already exists")
-                end
-            end, { desc = "create new note" })
-
-            vim.keymap.set("n", "<leader>nd", function()
-                obs.vault:open_daily()
-            end, { desc = "notes daily" })
-
-            vim.keymap.set("n", "<leader>nrn", function()
-                obs.vault:rename_current_note()
-            end, { desc = "notes rename current" })
-
-            vim.keymap.set("n", "<leader>nT", function()
-                obs.vault:run_if_note(function()
-                    obs.vault:find_and_insert_template()
-                end)
-            end, { desc = "Inserts notes Template" })
-
-            vim.keymap.set("n", "<leader>nM", function()
-                obs.vault:run_if_note(function()
-                    obs.vault:find_directory_and_move_current_note()
-                end)
-            end, { desc = "move notes to directory" })
-
-            vim.keymap.set("n", "<leader>nb", function()
-                obs.vault:run_if_note(function()
-                    obs.vault:find_current_note_backlinks()
-                end)
-            end, { desc = "notes find backlinks" })
-
-            -- Find stuff
-
-            vim.keymap.set("n", "<leader>nfj", function()
-                obs.vault:find_journal()
-            end, { desc = "notes find journal" })
-
-            vim.keymap.set("n", "<leader>nff", function()
-                obs.vault:find_note()
-            end, { desc = "notes files find" })
-
-            vim.keymap.set("n", "<leader>nfg", function()
-                obs.vault:grep_note()
-            end, { desc = "notes files grep" })
+            vim.keymap.set("n", "<leader>nn", "<cmd>ObsNvimFollowLink<cr>")
+            vim.keymap.set("n", "<leader>nN", "<cmd>ObsNvimNewNote<cr>")
+            vim.keymap.set("n", "<leader>nd", "<cmd>ObsNvimDailyNote<cr>")
+            vim.keymap.set("n", "<leader>nrn", "<cmd>ObsNvimRename<cr>")
+            vim.keymap.set("n", "<leader>nT", "<cmd>ObsNvimTemplate<cr>")
+            vim.keymap.set("n", "<leader>nM", "<cmd>ObsNvimMove<cr>")
+            vim.keymap.set("n", "<leader>nb", "<cmd>ObsNvimBacklinks<cr>")
+            vim.keymap.set("n", "<leader>nfj", "<cmd>ObsNvimFindInJournal<cr>")
+            vim.keymap.set("n", "<leader>nff", "<cmd>ObsNvimFindNote<cr>")
+            vim.keymap.set("n", "<leader>nfg", "<cmd>ObsNvimFindInNotes<cr>")
         end,
     },
 }
