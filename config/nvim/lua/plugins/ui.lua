c73cc5ca1bb44fcd6e934c9c7bedd91cec43f5d4
Ilia Ilinykh <ilyasyoy@gmail.com>
2023-08-28 23:09:31 +0300

    feat: ui autochange

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -53,10 +53,28 @@ return {
     "tjdevries/colorbuddy.nvim",
     {
         "ellisonleao/gruvbox.nvim",
-        lazy = false,
         config = function()
-            vim.cmd.colorscheme "gruvbox"
+            vim.api.nvim_set_option("background", "dark")
+            vim.cmd "colorscheme gruvbox"
         end,
+    },
+    {
+        "f-person/auto-dark-mode.nvim",
+        cond = false,
+        dependencies = {
+            "ellisonleao/gruvbox.nvim",
+        },
+        config = {
+            update_interval = 1000,
+            set_dark_mode = function()
+                vim.api.nvim_set_option("background", "dark")
+                vim.cmd "colorscheme gruvbox"
+            end,
+            set_light_mode = function()
+                vim.api.nvim_set_option("background", "light")
+                vim.cmd "colorscheme gruvbox"
+            end,
+        },
     },
     {
         "nvim-lualine/lualine.nvim",
