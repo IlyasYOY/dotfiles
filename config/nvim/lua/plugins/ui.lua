c72eb5f434e76740148ae7d990861d66c6bfcd48
Ilya Ilinykh <Ilyasyoy@gmail.com>
2023-09-06 19:08:55 +0300

    feat: change lualine

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -82,10 +82,12 @@ return {
             "IlyasYOY/coredor.nvim",
         },
         config = function()
+            local core = require "coredor"
             local lualine = require "lualine"
 
-            local function get_cwd()
-                return vim.fn.getcwd()
+            local is_jdtls_buffer = function()
+                local buf_path = vim.fn.expand "%"
+                return core.string_has_prefix(buf_path, "jdt", true)
             end
 
             lualine.setup {
@@ -95,7 +97,21 @@ return {
                 sections = {
                     lualine_a = { "mode" },
                     lualine_b = { "branch" },
-                    lualine_c = { get_cwd, "filename" },
+                    lualine_c = {
+                        {
+                            "filename",
+                            path = 3,
+                            cond = function()
+                                return not is_jdtls_buffer()
+                            end,
+                        },
+                        {
+                            "filename",
+                            cond = function()
+                                return is_jdtls_buffer()
+                            end,
+                        },
+                    },
                     lualine_x = { "fileformat", "filetype" },
                     lualine_y = { "progress" },
                     lualine_z = { "location" },
