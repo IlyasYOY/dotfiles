f5287ff067fd64323c0d75cc4b63703b9f5b2c85
Ilia Ilinykh <ilyasyoy@gmail.com>
2023-09-21 00:35:15 +0300

    feat: fzf in telescope

--- config/nvim/lua/plugins/search.lua
+++ config/nvim/lua/plugins/search.lua
@@ -23,7 +23,10 @@ return {
     {
         "nvim-telescope/telescope.nvim",
         version = "0.1.x",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        },
         config = function()
             local telescope = require "telescope"
             local builtin = require "telescope.builtin"
@@ -72,6 +75,7 @@ return {
                     },
                 },
             }
+            require("telescope").load_extension "fzf"
 
             vim.keymap.set("n", "<leader>ff", function()
                 builtin.find_files()
