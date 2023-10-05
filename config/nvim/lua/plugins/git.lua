f47cf1e12783e1943be224746171b519f1f2da02
Ilya Ilinykh <Ilyasyoy@gmail.com>
2023-10-05 16:34:08 +0300

    feat: added mappings

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -96,6 +96,42 @@ return {
                 "<cmd>Gitsigns blame_line<CR>",
                 { desc = "blame current line", noremap = true }
             )
+            vim.keymap.set(
+                "n",
+                "<leader>ga",
+                ":Gitsigns stage_hunk<cr>",
+                { desc = "stage hunk" }
+            )
+            vim.keymap.set(
+                "n",
+                "<leader>gA",
+                ":Gitsigns stage_buffer<cr>",
+                { desc = "stage buffer" }
+            )
+            vim.keymap.set(
+                "n",
+                "<leader>gr",
+                ":Gitsigns reset_hunk<cr>",
+                { desc = "reset hunk" }
+            )
+            vim.keymap.set(
+                "n",
+                "<leader>gR",
+                ":Gitsigns prev_buffer<cr>",
+                { desc = "reset buffer" }
+            )
+            vim.keymap.set(
+                "n",
+                "[g",
+                ":Gitsigns prev_hunk<cr>",
+                { desc = "go to prev hunk" }
+            )
+            vim.keymap.set(
+                "n",
+                "]g",
+                ":Gitsigns next_hunk<cr>",
+                { desc = "go to next hunk" }
+            )
         end,
     },
 }
