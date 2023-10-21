893249dae6978590eb2ab9676c15768c3a1e37e5
Ilia Ilinykh <ilyasyoy@gmail.com>
2023-10-21 22:56:28 +0300

    feat: ufo

--- config/nvim/lua/plugins/nvim-ufo.lua
+++ config/nvim/lua/plugins/nvim-ufo.lua
@@ -5,11 +5,41 @@ return {
             "kevinhwang91/promise-async",
             "nvim-treesitter/nvim-treesitter",
         },
-        cond = false,
         lazy = true,
         event = { "BufReadPre", "BufNewFile" },
         config = function()
+            local handler = function(virtText, lnum, endLnum, width, truncate)
+                local newVirtText = {}
+                local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+                local sufWidth = vim.fn.strdisplaywidth(suffix)
+                local targetWidth = width - sufWidth
+                local curWidth = 0
+                for _, chunk in ipairs(virtText) do
+                    local chunkText = chunk[1]
+                    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                    if targetWidth > curWidth + chunkWidth then
+                        table.insert(newVirtText, chunk)
+                    else
+                        chunkText = truncate(chunkText, targetWidth - curWidth)
+                        local hlGroup = chunk[2]
+                        table.insert(newVirtText, { chunkText, hlGroup })
+                        chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                        if curWidth + chunkWidth < targetWidth then
+                            suffix = suffix
+                                .. (" "):rep(
+                                    targetWidth - curWidth - chunkWidth
+                                )
+                        end
+                        break
+                    end
+                    curWidth = curWidth + chunkWidth
+                end
+                table.insert(newVirtText, { suffix, "MoreMsg" })
+                return newVirtText
+            end
+
             require("ufo").setup {
+                fold_virt_text_handler = handler,
                 provider_selector = function(bufnr, filetype, buftype)
                     return { "treesitter", "indent" }
                 end,
@@ -22,6 +52,12 @@ return {
 
             vim.keymap.set("n", "zR", require("ufo").openAllFolds)
             vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+            vim.keymap.set("n", "<leader>k", function()
+                local winid = require("ufo").peekFoldedLinesUnderCursor()
+                if not winid then
+                    vim.lsp.buf.hover()
+                end
+            end)
         end,
     },
 }
