7cb5ad01351abad124aa9314ae9e45dcb7e89152
Ilia Ilinykh <ilyasyoy@gmail.com>
2023-10-25 01:01:12 +0300

    feat: java stuff

--- config/nvim/lua/plugins/null-ls.lua
+++ config/nvim/lua/plugins/null-ls.lua
@@ -1,9 +1,8 @@
 return {
     {
-        "nvimtools/none-ls.nvim",
+        "jose-elias-alvarez/null-ls.nvim",
         config = function()
             local null_ls = require "null-ls"
-            -- local pmd_cpd_source = require "ilyasyoy.null-ls.pmd-cpd"
             local core = require "ilyasyoy.functions.core"
 
             local function with_root_file(builtin, file)
