7e1b08e95a13603f4cb5f9c87ef09a44358d581d
Ilya Ilyinykh <ilyasyoy@gmail.com>
2023-11-23 17:25:29 +0300

    refactor: java

--- config/nvim/after/ftplugin/java.lua
+++ config/nvim/after/ftplugin/java.lua
@@ -216,13 +216,6 @@ local config = {
         end, { desc = "java debug nearest test class" })
         vim.keymap.set("n", "<leader>jr", "<cmd>JdtWipeDataAndRestart<CR>", { desc = "restart jdtls" })
 
-        vim.cmd [[ command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>) ]]
-        vim.cmd [[ command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>) ]]
-        vim.cmd [[ command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config() ]]
-        vim.cmd [[ command! -buffer JdtBytecode lua require('jdtls').javap() ]]
-        vim.cmd [[ command! -buffer JdtJol lua require('jdtls').jol() ]]
-        vim.cmd [[ command! -buffer JdtJshell lua require('jdtls').jshell() ]]
-
         lsp.on_attach(client, bufnr)
     end,
     capabilities = lsp.get_capabilities(),
