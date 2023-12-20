87232d2a23fe315d3c8bdc363a5397d2cebbcff7
Ilya Ilyinykh <IlyasYOY@gmail.com>
2023-12-21 00:50:05 +0300

    refactor: colors

--- config/nvim/lua/plugins/colorbuddy-nvim.lua
+++ config/nvim/lua/plugins/colorbuddy-nvim.lua
@@ -1,6 +1,9 @@
 return {
     {
         "tjdevries/colorbuddy.nvim",
+        dependencies = {
+            "ellisonleao/gruvbox.nvim",
+        },
         config = function()
             vim.cmd "colorscheme gruvbox"
             local Color, colors, Group, groups, styles =
@@ -9,8 +12,8 @@ return {
             Color.new("red", "#cc3333")
             Color.new("green", "#33cc33")
 
-            Group.new("@text.todo.unchecked", colors.red, nil, styles.bold)
-            Group.new("@text.todo.checked", colors.green, nil, styles.bold)
+            Group.new("@text.todo.unchecked", colors.red, colors.none, styles.none)
+            Group.new("@text.todo.checked", colors.green, colors.none, styles.none)
         end,
     },
 }
