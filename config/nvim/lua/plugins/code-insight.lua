1f8d339b4712abfdc838fd486d6684f8760d8aa4
Ilia Ilinykh <ilyasyoy@gmail.com>
2024-01-09 16:29:39 +0300

    chore: remove class movements

--- config/nvim/lua/plugins/code-insight.lua
+++ config/nvim/lua/plugins/code-insight.lua
@@ -234,19 +234,15 @@ return {
                         set_jumps = true,
                         goto_next_start = {
                             ["]m"] = "@function.outer",
-                            ["]c"] = "@class.outer",
                         },
                         goto_next_end = {
                             ["]M"] = "@function.outer",
-                            ["]C"] = "@class.outer",
                         },
                         goto_previous_start = {
                             ["[m"] = "@function.outer",
-                            ["[c"] = "@class.outer",
                         },
                         goto_previous_end = {
                             ["[M"] = "@function.outer",
-                            ["[C"] = "@class.outer",
                         },
                     },
                     swap = {
