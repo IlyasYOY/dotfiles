1af494617e73c51e68bb5580f2d5631ccb19a482
Ilya Ilyinykh <IlyasYOY@gmail.com>
2024-03-18 17:06:26 +0300

    feat: update tmux & colors

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -5,15 +5,32 @@ return {
             "ellisonleao/gruvbox.nvim",
         },
         config = function()
-            vim.cmd "colorscheme gruvbox"
-            local Color, colors, Group, groups, styles =
-                require("colorbuddy").setup()
+            vim.cmd.colorscheme "gruvbox"
+
+            local colorbuddy = require "colorbuddy"
+
+            -- And then modify as you like
+            local Color = colorbuddy.Color
+            local colors = colorbuddy.colors
+            local Group = colorbuddy.Group
+            local groups = colorbuddy.groups
+            local styles = colorbuddy.styles
 
             Color.new("red", "#cc3333")
             Color.new("green", "#33cc33")
 
-            Group.new("@markup.list.unchecked.markdown", colors.red, colors.none, styles.none)
-            Group.new("@markup.list.checked.markdown", colors.green, colors.none, styles.none)
+            Group.new(
+                "@markup.list.unchecked.markdown",
+                colors.red,
+                colors.none,
+                styles.none
+            )
+            Group.new(
+                "@markup.list.checked.markdown",
+                colors.green,
+                colors.none,
+                styles.none
+            )
         end,
     },
 }
