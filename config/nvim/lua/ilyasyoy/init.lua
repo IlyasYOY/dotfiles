aa94afdb1ae5be04beded774a1146acc805ed34c
Ilya Ilyinykh <IlyasYOY@gmail.com>
2024-05-17 13:30:15 +0300

    feat: remove laststatus

--- config/nvim/lua/ilyasyoy/init.lua
+++ config/nvim/lua/ilyasyoy/init.lua
@@ -4,7 +4,6 @@ vim.g.netrw_banner = 0 -- Now we won't have bloated top of the window
 vim.g.netrw_liststyle = 3 -- Now it will be a tree view
 vim.g.netrw_bufsettings = "nu nobl"
 
-vim.opt.laststatus = 3
 vim.cmd "highlight WinSeparator guibg=None"
 
 vim.opt.spelllang = "ru_ru,en_us"
