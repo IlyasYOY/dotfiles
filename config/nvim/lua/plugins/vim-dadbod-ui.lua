8c0044a88ec52426f754f3fe26e71e72c2299ceb
Ilya Ilyinykh <IlyasYOY@gmail.com>
2024-09-02 17:23:42 +0300

    feat: dadbod postgres request

--- config/nvim/lua/plugins/vim-dadbod-ui.lua
+++ config/nvim/lua/plugins/vim-dadbod-ui.lua
@@ -6,6 +6,18 @@ return {
         ft = { "sql", "mysql", "plsql" },
         config = function()
             vim.g.db_ui_save_location = vim.fn.getcwd() .. "/sql/"
+            vim.g.db_ui_table_helpers = {
+                postgresql = {
+                    ["Table Size"] = [[
+select
+    table_name,
+    pg_size_pretty(pg_total_relation_size(quote_ident(table_name))),
+    pg_total_relation_size(quote_ident(table_name))
+from information_schema.tables
+where table_schema = 'public' and table_name = '{table}';
+]],
+                },
+            }
         end,
         dependencies = {
             "tpope/vim-dadbod",
