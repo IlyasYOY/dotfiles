6b86590dae94eb9ed068cee250865a42b3c6367d
Ilya Ilyinykh <IlyasYOY@gmail.com>
2024-09-22 23:04:38 +0300

    fix: dap

--- config/nvim/lua/plugins/nvim-dap-ui.lua
+++ config/nvim/lua/plugins/nvim-dap-ui.lua
@@ -3,6 +3,7 @@ return {
         "rcarriga/nvim-dap-ui",
         dependencies = {
             "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
         },
         keys = {
             "<leader>Du",
