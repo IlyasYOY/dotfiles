bb372b596623c613b7ddcdeadc1227c876206b1a
Ilya Ilyinykh <IlyasYOY@gmail.com>
2024-12-13 17:52:26 +0300

    fix: constructor color

--- config/nvim/lua/plugins/colorboddy-nvim.lua
+++ config/nvim/lua/plugins/colorboddy-nvim.lua
@@ -131,7 +131,7 @@ return {
             -- treesitter stuff
             Group.link("@type.builtin", g.User3)
             Group.link("@constant.builtin", g.User1)
-            Group.link("@constructor", g.Normal)
+            Group.link("@constructor", g.Special)
             Group.link("@exception.operator", g.Special)
             Group.link("@function.macro", g.Normal)
             Group.link("@namespace", g.Normal)
