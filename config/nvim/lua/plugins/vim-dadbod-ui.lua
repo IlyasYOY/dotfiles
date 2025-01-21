729c29743d78cf5643538aecf4a963cc03a8368a
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-01-21 17:27:41 +0300

    feat: add count in dadbod-ui

--- config/nvim/lua/plugins/vim-dadbod-ui.lua
+++ config/nvim/lua/plugins/vim-dadbod-ui.lua
@@ -16,6 +16,9 @@ select
 from information_schema.tables
 where table_schema = 'public' and table_name = '{table}';
 ]],
+                    ["Count"] = [[
+select count(*) from {table};
+]],
                 },
             }
         end,
