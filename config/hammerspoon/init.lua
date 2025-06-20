f5de30043cee867b481cb9707bb217493d760058
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-06-20 16:38:19 +0300

    fix(hammerspoon): swap shortcuts for TickTick and Obsidian

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,9 +1,9 @@
 for _, app in ipairs {
     { shortcut = "1", name = "WezTerm" },
     { shortcut = "2", name = "Google Chrome" },
-    { shortcut = "3", name = "TickTick" },
+    { shortcut = "3", name = "Obsidian" },
     { shortcut = "4", name = "Telegram" },
-    { shortcut = "5", name = "Obsidian" },
+    { shortcut = "5", name = "TickTick" },
 } do
     hs.hotkey.bind({ "alt" }, app.shortcut, function()
         hs.application.launchOrFocus(app.name)
