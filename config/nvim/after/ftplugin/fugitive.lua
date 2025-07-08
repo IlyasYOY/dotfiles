c3c423bbb2861a4933663afd05c2dab14c5bab2f
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-07-08 15:19:02 +0300

    feat(config/nvim/after/ftplugin/fugitive.lua): add keymap for soft reset on current word

--- config/nvim/after/ftplugin/fugitive.lua
+++ config/nvim/after/ftplugin/fugitive.lua
@@ -1,3 +1,9 @@
 vim.keymap.set("n", "dt", ":Gtabedit <Plug><cfile><Bar>Gvdiffsplit<CR>", {
     buffer = true,
 })
+
+vim.keymap.set(
+    "n",
+    "<localleader>rs",
+    [[<cmd>execute ':Git reset --soft ' . expand('<cWORD>') <CR>]]
+)
