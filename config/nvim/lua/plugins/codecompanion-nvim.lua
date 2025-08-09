962bf1adb0955dd0f5694effefe386bf95736562
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-08-09 17:16:58 +0300

    refactor: update yandex api keys and remove unused prompts

--- config/nvim/lua/plugins/codecompanion-nvim.lua
+++ config/nvim/lua/plugins/codecompanion-nvim.lua
@@ -48,7 +48,7 @@ return {
                         adapter = {
                             name = "yandexgpt",
                             model = "gpt://"
-                                .. pass.load_secret "cloud/yandex/ilyasyoy-catalog-id"
+                                .. pass.load_secret "cloud/yandex/catalog-id"
                                 .. "/qwen3-235b-a22b-fp8/latest",
                         },
                     },
@@ -56,7 +56,7 @@ return {
                         adapter = {
                             name = "yandexgpt",
                             model = "gpt://"
-                                .. pass.load_secret "cloud/yandex/ilyasyoy-catalog-id"
+                                .. pass.load_secret "cloud/yandex/catalog-id"
                                 .. "/llama",
                         },
                     },
@@ -73,7 +73,7 @@ return {
                             {
                                 env = {
                                     url = "https://llm.api.cloud.yandex.net",
-                                    api_key = pass.load_secret "cloud/yandex/ilyasyoy-ai-api-key",
+                                    api_key = pass.load_secret "cloud/yandex/llm-api-key",
                                 },
                             }
                         )
@@ -102,37 +102,6 @@ return {
                                 visible = false,
                                 tag = "system_tag",
                             },
-                        },
-                    },
-                },
-                ["Review Diff"] = {
-                    strategy = "chat",
-                    description = "Generate a commit message",
-                    opts = {
-                        is_slash_cmd = true,
-                        short_name = "review-diff",
-                        auto_submit = true,
-                    },
-                    prompts = {
-                        {
-                            role = "user",
-                            content = function()
-                                return string.format(
-                                    [[
-I want you to act as a Code reviewer who is experienced developer in the given
-code language. I will provide you with the code block or methods or code file
-along with the code language name, and I would like you to review the code and
-share the feedback, suggestions and alternative recommended approaches. Please
-write explanations behind the feedback or suggestions or alternative
-approaches.
-
-```diff
-%s
-```
-                                    ]],
-                                    vim.fn.system "git diff --no-ext-diff --staged"
-                                )
-                            end,
                         },
                     },
                 },
@@ -281,47 +250,6 @@ Wrap errors in this code:
                         end,
                         opts = {
                             contains_code = true,
-                        },
-                    },
-                },
-                ["Text Refine Prompt Chat"] = {
-                    strategy = "chat",
-                    description = "Refines prompts",
-                    opts = {
-                        ignore_system_prompt = true,
-                    },
-                    prompts = {
-                        {
-                            role = "system",
-                            content = [[
-As a professional Prompt Engineer, your role is to create effective and innovative prompts for interacting with AI models.
-
-Your core skills include:
-1. **CO-STAR Framework Application**: Utilize the CO-STAR framework to build efficient prompts, ensuring effective communication with large language models.
-2. **Contextual Awareness**: Construct prompts that adapt to complex conversation contexts, ensuring relevant and coherent responses.
-3. **Chain-of-Thought Prompting**: Create prompts that elicit AI models to demonstrate their reasoning process, enhancing the transparency and accuracy of answers.
-4. **Zero-shot Learning**: Design prompts that enable AI models to perform specific tasks without requiring examples, reducing dependence on training data.
-5. **Few-shot Learning**: Guide AI models to quickly learn and execute new tasks through a few examples.
-
-Your output format should include:
-- **Context**: Provide comprehensive background information for the task to ensure the AI understands the specific scenario and offers relevant feedback.
-- **Objective**: Clearly define the task objective, guiding the AI to focus on achieving specific goals.
-- **Style**: Specify writing styles according to requirements, such as imitating a particular person or industry expert.
-- **Tone**: Set an appropriate emotional tone to ensure the AI's response aligns with the expected emotional context.
-- **Audience**: Tailor AI responses for a specific audience, ensuring content appropriateness and ease of understanding.
-- **Response**: Specify output formats for easy execution of downstream tasks, such as lists, JSON, or professional reports.
-- **Workflow**: Instruct the AI on how to step-by-step complete tasks, clarifying inputs, outputs, and specific actions for each step.
-- **Examples**: Show a case of input and output that fits the scenario.
-
-Your workflow should be:
-1. Extract key information from user requests to determine design objectives.
-2. Based on user needs, create prompts that meet requirements, with each part being professional and detailed.
-3. Must only output the newly generated and optimized prompts, without explanation, without wrapping it in markdown code block.
-]],
-                        },
-                        {
-                            role = "user",
-                            content = "I want to...",
                         },
                     },
                 },
