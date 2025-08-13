4ca6b7369ebc9d92adb8f20637c2ca58ab589470
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-08-14 00:23:57 +0300

    feat: update application shortcuts

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -2,8 +2,9 @@ for _, app in ipairs {
     { shortcut = "1", name = "WezTerm" },
     { shortcut = "2", name = "Google Chrome" },
     { shortcut = "3", name = "Obsidian" },
-    { shortcut = "4", name = "Telegram" },
-    { shortcut = "5", name = "Calendar" },
+    { shortcut = "8", name = "Telegram" },
+    { shortcut = "9", name = "Calendar" },
+    { shortcut = "0", name = "Mail" },
 } do
     hs.hotkey.bind({ "alt" }, app.shortcut, function()
         hs.application.launchOrFocus(app.name)
