a3248b30dbb2a642176e4d9efa13490cf726a750
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-08-14 00:23:45 +0300

    feat: configure fidget-nvim notification window

--- config/nvim/lua/plugins/fidget-nvim.lua
+++ config/nvim/lua/plugins/fidget-nvim.lua
@@ -1,6 +1,13 @@
 return {
     {
         "j-hui/fidget.nvim",
-        opts = {},
+        opts = {
+            notification = {
+                window = {
+                    winblend = 30,     -- Background color opacity in the notification window
+                    border = "single", -- Border around the notification window
+                },
+            },
+        },
     },
 }
