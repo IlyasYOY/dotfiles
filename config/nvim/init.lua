e86ddb61943d5f2da24ce58e7d60c47e13f584a3
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-08-15 23:56:15 +0300

    feat(config): add guicursor option to init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,6 @@
 -- TODO: Remove this after: https://github.com/neovim/neovim/issues/31675
 vim.hl = vim.highlight
+vim.opt.guicursor = "n:block-Cursor"
 
 -- Here I load files with custom settings for machine.
 -- This lua file is hidden from VCS, so I can do tricky stuff there.
