39a6e1c43116447b2bb3cb7b295533238d3d130b
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-08-17 13:24:54 +0300

    refactor: simplify keymaps and remove unused functionality

--- config/nvim/lua/ilyasyoy/init.lua
+++ config/nvim/lua/ilyasyoy/init.lua
@@ -14,60 +14,11 @@ vim.diagnostic.config { virtual_text = true }
 
 -- Dev things
 
-vim.keymap.set("n", "<leader><leader>d", function()
+vim.keymap.set("n", "<leader>D", function()
     vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-end, { desc = "Toggle diagnostic virtual_lines" })
+end, { desc = "Toggle diagnostic" })
 
-vim.keymap.set(
-    "n",
-    "<leader>Dt",
-    "<cmd>diffthis<CR>",
-    { desc = "diff this file" }
-)
-
-vim.keymap.set(
-    "n",
-    "<leader>Do",
-    "<cmd>diffoff<CR>",
-    { desc = "diff off file" }
-)
-
-vim.keymap.set(
-    "n",
-    "<leader><leader>S",
-    "<cmd>mksession!<CR>",
-    { desc = "create session file" }
-)
-
--- Create a variable to track the state
-vim.g.auto_refresh_enabled = false
-
--- Function to toggle the behavior
-local function toggle_auto_refresh()
-    if vim.g.auto_refresh_enabled then
-        vim.api.nvim_clear_autocmds { group = "AutoRefresh" }
-        vim.g.auto_refresh_enabled = false
-        print "Auto refresh disabled"
-    else
-        vim.o.autoread = true
-        vim.api.nvim_create_augroup("AutoRefresh", { clear = true })
-        vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
-            group = "AutoRefresh",
-            command = "if mode() != 'c' | checktime | endif",
-            pattern = "*",
-        })
-        vim.g.auto_refresh_enabled = true
-        print "Auto refresh enabled"
-    end
-end
-
-vim.keymap.set("n", "<leader><leader>ar", toggle_auto_refresh, {
-    noremap = true,
-    silent = false,
-    desc = "Toggle auto refresh of files",
-})
-
-vim.keymap.set("n", "<leader><leader>sc", function()
+vim.keymap.set("n", "<localleader>sc", function()
     vim.opt_local.spell = not (vim.opt_local.spell:get())
     vim.notify("spell: " .. tostring(vim.o.spell))
 end, { desc = "Toggle spell check" })
