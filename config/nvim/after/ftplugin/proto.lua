60ef69fc8a1cc2458f4c44b3e7e09bd40b71e2ea
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-09-03 17:28:03 +0300

    fix: update command creation to use nvim_buf_create_user_command

--- config/nvim/after/ftplugin/proto.lua
+++ config/nvim/after/ftplugin/proto.lua
@@ -3,7 +3,8 @@ vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
 
-vim.api.nvim_create_user_command(
+vim.api.nvim_buf_create_user_command(
+    0,
     "ProtoAddJSONTag",
     [[%s/\(\w* \(\w*\) = \d\+\);/\1 [json_name = "\2"];/gc]],
     {
