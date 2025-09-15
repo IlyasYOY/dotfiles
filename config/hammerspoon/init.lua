0edfb80975697ecf45f4703a36ab2a5ff7e7374f
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-09-15 18:16:31 +0300

    fix: update Hammerspoon init.lua to change Obsidian shortcut and add TickTick

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,7 +1,8 @@
 for _, app in ipairs {
     { shortcut = "1", name = "WezTerm" },
     { shortcut = "2", name = "Google Chrome" },
-    { shortcut = "3", name = "Obsidian" },
+    { shortcut = "3", name = "TickTick" },
+    { shortcut = "4", name = "Obsidian" },
     { shortcut = "8", name = "Telegram" },
     { shortcut = "9", name = "Calendar" },
     { shortcut = "0", name = "Mail" },
