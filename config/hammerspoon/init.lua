d2f61f00347a597886f9dfa420e108efe84acc7e
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-09-17 01:11:48 +0300

    chore(config): update app entry from TickTick to Todoist in hammerspoon init.lua

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,7 +1,7 @@
 for _, app in ipairs {
     { shortcut = "1", name = "WezTerm" },
     { shortcut = "2", name = "Google Chrome" },
-    { shortcut = "3", name = "TickTick" },
+    { shortcut = "3", name = "Todoist" },
     { shortcut = "4", name = "Obsidian" },
     { shortcut = "8", name = "Telegram" },
     { shortcut = "9", name = "Calendar" },
