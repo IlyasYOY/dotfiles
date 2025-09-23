0aa8c3656cf25a9562798a2a0394cd7addab22a1
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-09-23 17:07:41 +0300

    chore(config): enable wrap and enforce 72‑character line length in gitcommit buffers
    add local options to gitcommit filetype: enable spell checking, enable line wrapping, set textwidth to 72, and display a color column at 73 to guide commit message length.

--- config/nvim/after/ftplugin/gitcommit.lua
+++ config/nvim/after/ftplugin/gitcommit.lua
@@ -1,4 +1,7 @@
 vim.opt_local.spell = true
+vim.opt_local.wrap = true
+vim.opt_local.textwidth = 72
+vim.opt_local.colorcolumn = "73"
 
 require("cmp").setup.buffer {
     sources = require("cmp").config.sources(
