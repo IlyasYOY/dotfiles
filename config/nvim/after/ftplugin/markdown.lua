9070db4be0f482cf9abfa9bd37fb01dbf9130ded
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-09-26 00:17:08 +0300

    feat(markdown): add AIChatMDCreateTitle command for title generation
    Introduce a buffer-local user command that runs `aichat` to generate a title for the current markdown buffer, improving workflow for creating document headings.

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,9 @@
 vim.opt_local.spell = true
 vim.opt_local.wrap = true
+
+vim.api.nvim_buf_create_user_command(0, "AIChatMDCreateTitle", function()
+    vim.cmd "r ! cat % | aichat --code --role \\%create-title\\%"
+end, {
+    range = true,
+    desc = "Create a title for the current markdown buffer using AIChat",
+})
