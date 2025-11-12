ac3a786864c704af04b307d0509d5e33181eab7d
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-11-12 23:40:45 +0300

    feat(config): add visual mode copy path with line numbers
    Add a visual mode mapping <leader>cp that copies the relative file path with the selected line range as a link. Reformat the normal mode copy mapping for consistency and adjust whitespace for alignment.

--- config/nvim/lua/ilyasyoy/init.lua
+++ config/nvim/lua/ilyasyoy/init.lua
@@ -1,6 +1,6 @@
 vim.cmd "source ~/.vimrc"
 
-vim.g.netrw_banner = 0 -- Now we won't have bloated top of the window
+vim.g.netrw_banner = 0    -- Now we won't have bloated top of the window
 vim.g.netrw_liststyle = 3 -- Now it will be a tree view
 vim.g.netrw_bufsettings = "nu nobl"
 
@@ -23,8 +23,25 @@ vim.keymap.set("n", "<localleader>sc", function()
     vim.notify("spell: " .. tostring(vim.o.spell))
 end, { desc = "Toggle spell check" })
 
-vim.keymap.set('n', '<leader>cp', function()
-    local path = vim.fn.expand('%:.')
-    vim.fn.setreg('+', path)
-    vim.notify('Copied: ' .. path)
-end, { desc = 'Copy relative file path to clipboard' })
+vim.keymap.set("n", "<leader>cp", function()
+    local path = vim.fn.expand "%:."
+    vim.fn.setreg("+", path)
+    vim.notify("Copied: " .. path)
+end, { desc = "Copy relative file path to clipboard" })
+
+vim.keymap.set("v", "<leader>cp", function()
+    vim.api.nvim_feedkeys(
+        vim.api.nvim_replace_termcodes("<Esc>", true, false, true),
+        "nx", -- 'n' for normal mode, 'x' to update '<' and '>' marks correctly
+        false
+    )
+    local start_line = vim.fn.line "'<"
+    local end_line = vim.fn.line "'>"
+    local path = vim.fn.expand "%:."
+    local link = path
+    if start_line ~= end_line then
+        link = link .. "#" .. start_line .. "-" .. end_line
+    end
+    vim.fn.setreg("+", link)
+    vim.notify("Copied: " .. link)
+end, { desc = "Copy relative file path with line numbers to clipboard" })
