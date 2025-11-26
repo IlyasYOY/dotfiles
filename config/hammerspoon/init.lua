2c2b989a4bafa2858e1b0ea0a9cf225c63d4ad5a
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-11-26 10:07:32 +0300

    chore(config): replace Todoist with Reminders for shortcut 3
    Update Hammerspoon app launcher configuration to open Reminders instead of Todoist when pressing the assigned shortcut.

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,7 +1,8 @@
 for _, app in ipairs {
     { shortcut = "1", name = "WezTerm" },
     { shortcut = "2", name = "Google Chrome" },
-    { shortcut = "3", name = "Todoist" },
+    { shortcut = "3", name = "Reminders" },
+
     { shortcut = "4", name = "Obsidian" },
     { shortcut = "8", name = "Telegram" },
     { shortcut = "9", name = "Calendar" },
