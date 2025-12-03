9a16f47add75a1869e62a8d7147ececda2eb6bb9
Ilya Ilyinykh <IlyasYOY@gmail.com>
2025-12-03 22:27:33 +0300

    chore(setup): update local repo list with new projects and reorder entries
    Add new repositories (detailederror, ffmpeg-video-chapters-parser, IlyasYOY, go-retry, httpservertest, tasks-assistant-telegram-bot) to the update script and move obs.nvim and theme.nvim to the end of the list for clearer organization.

--- sh/setup/update.sh
+++ sh/setup/update.sh
@@ -19,10 +19,17 @@ update_nvim() {
 
 update_local_repos() {
     update_repo "$PERSONAL_PROJECTS_DIR/dotfiles"
-    update_repo "$PERSONAL_PROJECTS_DIR/theme.nvim"
-    update_repo "$PERSONAL_PROJECTS_DIR/obs.nvim"
-    update_repo "$PERSONAL_PROJECTS_DIR/coredor.nvim"
+
+    update_repo "$PERSONAL_PROJECTS_DIR/detailederror"
+    update_repo "$PERSONAL_PROJECTS_DIR/ffmpeg-video-chapters-parser"
+    update_repo "$PERSONAL_PROJECTS_DIR/IlyasYOY"
+    update_repo "$PERSONAL_PROJECTS_DIR/go-retry"
+    update_repo "$PERSONAL_PROJECTS_DIR/httpservertest"
+    update_repo "$PERSONAL_PROJECTS_DIR/tasks-assistant-telegram-bot"
+
     update_repo "$PERSONAL_PROJECTS_DIR/git-link.nvim"
+    update_repo "$PERSONAL_PROJECTS_DIR/obs.nvim"
+    update_repo "$PERSONAL_PROJECTS_DIR/theme.nvim"
 
     update_repo "$NOTES_DIR"
 
