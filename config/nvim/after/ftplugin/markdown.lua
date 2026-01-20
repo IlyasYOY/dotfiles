198493028367c008309acd570637302fe2e8fd31
Ilya Ilyinykh <IlyasYOY@gmail.com>
2026-01-20 13:46:55 +0300

    feat(markdown): add visual mode formatter keybindings
    Introduce `setup_formatters` to define visual‑mode mappings for italic (`<localleader>fi`), bold (`<localleader>fb`), code (`<localleader>fc`), and link (`<localleader>fl`). Register the function and invoke it in the markdown ftplugin to enable quick markdown formatting.

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -2,6 +2,37 @@ vim.opt_local.spell = true
 vim.opt_local.wrap = true
 vim.bo.formatprg = "mdsf format --stdin --log-level=off"
 
+local function setup_formatters()
+    vim.keymap.set("v", "<localleader>fi", function()
+        return "c*<C-r>-*<Esc>"
+    end, {
+        expr = true,
+        buffer = true,
+        desc = "Wrap selected text with italic markers (*)",
+    })
+    vim.keymap.set("v", "<localleader>fb", function()
+        return "c**<C-r>-**<Esc>"
+    end, {
+        expr = true,
+        buffer = true,
+        desc = "Wrap selected text with bold markers (**)",
+    })
+    vim.keymap.set("v", "<localleader>fc", function()
+        return "c`<C-r>-`<Esc>"
+    end, {
+        expr = true,
+        buffer = true,
+        desc = "Wrap selected text with code markers (`)",
+    })
+    vim.keymap.set("v", "<localleader>fl", function()
+        return "c[<C-r>-](<C-r>+)<Esc>"
+    end, {
+        expr = true,
+        buffer = true,
+        desc = "Wrap selected text with link ([]()), link valus is + register",
+    })
+end
+
 local function setup_ai()
     vim.api.nvim_buf_create_user_command(0, "AIChatMDCreateTitle", function()
         local filename = vim.api.nvim_buf_get_name(0)
@@ -28,8 +59,9 @@ local function setup_ai()
         )
     end, {
         range = false,
-        desc = "Create a title for the current markdown buffer using AIChat and copy it to the \" register",
+        desc = 'Create a title for the current markdown buffer using AIChat and copy it to the " register',
     })
 end
 
 setup_ai()
+setup_formatters()
