cacc7723aa8e2e5ef128dea3f73e94f07358fcb8
Ilya Ilyinykh <IlyasYOY@gmail.com>
2026-02-02 14:16:22 +0300

    chore(hammerspoon): rename app 4 to SingularityApp
    update shortcut mapping to launch SingularityApp for shortcut "4" instead of Google Tasks

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -2,7 +2,7 @@ for _, app in ipairs {
     { shortcut = "1", name = "WezTerm" },
     { shortcut = "2", name = "Google Chrome" },
     { shortcut = "3", name = "Obsidian" },
-    { shortcut = "4", name = "Google Tasks" },
+    { shortcut = "4", name = "SingularityApp" },
     { shortcut = "5", name = "Final Cut Pro" },
 
     { shortcut = "8", name = "Telegram" },
