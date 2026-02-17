ed92663f76254efc1e09e0a3ebefa1009665d2af
Ilya Ilyinykh <IlyasYOY@gmail.com>
2026-02-17 14:55:14 +0300

    feat: add copilot support for gitcommit filetype and add a keymap for committing in fugitive buffers

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -2,5 +2,18 @@ return {
     {
         "github/copilot.vim",
         event = "VeryLazy",
+        config = function()
+            vim.g.copilot_filetypes = {
+                ["*"] = false,
+
+                lua = true,
+                gitcommit = true,
+                python = true,
+                go = true,
+                java = true,
+                make = true,
+                sh = true,
+            }
+        end
     }
 }
