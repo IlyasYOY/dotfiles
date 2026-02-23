c54a5de1c690e091c3e7e11310334bcb2a3b9d4b
Ilya Ilyinykh <IlyasYOY@gmail.com>
2026-02-24 00:36:43 +0300

    feat: Add keymaps for Copilot

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -14,6 +14,10 @@ return {
                 make = true,
                 sh = true,
             }
+
+            vim.keymap.set("n", "<leader>aa", "<cmd>Copilot panel<CR>", { desc = "Open Copilot Panel" })
+            vim.keymap.set("n", "<leader>ae", "<cmd>Copilot enable<CR>", { desc = "Enable Copilot" })
+            vim.keymap.set("n", "<leader>ad", "<cmd>Copilot disable<CR>", { desc = "Disable Copilot" })
         end
     }
 }
