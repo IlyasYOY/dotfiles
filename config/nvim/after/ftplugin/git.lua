19a37b61cf7bbfb55c03cda3f83a6c4ef9171fe0
Ilya Ilyinykh <IlyasYOY@gmail.com>
2026-03-12 15:16:51 +0300

    feat: update git and gitcommit ftplugins

--- config/nvim/after/ftplugin/git.lua
+++ config/nvim/after/ftplugin/git.lua
@@ -1,7 +1,7 @@
 vim.opt_local.foldmethod = "syntax"
 vim.opt_local.foldenable = true
-vim.opt_local.foldlevel = 0
-vim.opt_local.foldlevelstart = 0
+vim.opt_local.foldlevel = 99
+vim.opt_local.foldlevelstart = 99
 
 vim.keymap.set(
     "n",
--- config/nvim/after/ftplugin/gitcommit.lua
+++ config/nvim/after/ftplugin/gitcommit.lua
@@ -4,6 +4,6 @@ vim.opt_local.textwidth = 72
 vim.opt_local.colorcolumn = "73"
 
 vim.opt_local.foldmethod = "syntax"
-vim.opt_local.foldlevel = 0
-vim.opt_local.foldlevelstart = 0
 vim.opt_local.foldenable = true
+vim.opt_local.foldlevel = 99
+vim.opt_local.foldlevelstart = 99
