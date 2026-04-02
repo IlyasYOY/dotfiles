f66c39578ac1be957a8c8078e5781a5a91c570f9
Ilya Ilyinykh <IlyasYOY@gmail.com>
2026-04-02 22:27:41 +0300

    fix: Schedule luasnip actions to avoid "Already inside a Lua callback" error

--- config/nvim/after/plugin/luasnip.lua
+++ config/nvim/after/plugin/luasnip.lua
@@ -4,47 +4,54 @@ ls.config.set_config {
     update_events = "TextChanged,TextChangedI",
 }
 
+local function schedule_luasnip(action)
+    vim.schedule(action)
+    return ""
+end
+
 vim.keymap.set({ "i" }, "<C-j>", function()
     if ls.expandable() then
-        vim.schedule(function()
+        return schedule_luasnip(function()
             ls.expand()
         end)
     elseif ls.locally_jumpable(1) then
-        vim.schedule(function()
+        return schedule_luasnip(function()
             ls.jump(1)
         end)
-    else
-        return "<C-j>"
     end
+
+    return "<C-j>"
 end, { expr = true, silent = true })
 
 vim.keymap.set({ "i", "s" }, "<C-k>", function()
     if ls.jumpable(-1) then
-        ls.jump(-1)
-    else
-        return "<C-k>"
+        return schedule_luasnip(function()
+            ls.jump(-1)
+        end)
     end
+
+    return "<C-k>"
 end, { expr = true, silent = true })
 
-vim.keymap.set("i", "<C-l>", function()
+vim.keymap.set({ "i", "s" }, "<C-l>", function()
     if ls.choice_active() then
-        ls.change_choice(1)
-    else
-        return "<C-l>"
+        return schedule_luasnip(function()
+            ls.change_choice(1)
+        end)
     end
-end, {
-    expr = true,
-})
 
-vim.keymap.set("i", "<C-h>", function()
+    return "<C-l>"
+end, { expr = true, silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-h>", function()
     if ls.choice_active() then
-        ls.change_choice(-1)
-    else
-        return "<C-h>"
+        return schedule_luasnip(function()
+            ls.change_choice(-1)
+        end)
     end
-end, {
-    expr = true,
-})
+
+    return "<C-h>"
+end, { expr = true, silent = true })
 
 vim.defer_fn(function()
     require("luasnip.loaders.from_snipmate").lazy_load()
