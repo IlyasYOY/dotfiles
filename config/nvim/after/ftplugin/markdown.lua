a3310abc84adb25e7f4b94ed0e97daaa835adc25
Ilya Ilyinykh <IlyasYOY@gmail.com>
2026-04-21 17:30:26 +0300

    feat(nvim): move markdown toc to left sidebar

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -5,6 +5,22 @@ vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.bo.formatprg = "mdsf format --stdin --log-level=off"
 
+local function show_toc_side()
+    local current_win = vim.api.nvim_get_current_win()
+
+    require("vim.treesitter._headings").show_toc()
+
+    if
+        vim.api.nvim_get_current_win() == current_win
+        or vim.bo.filetype ~= "qf"
+    then
+        return
+    end
+
+    vim.cmd.wincmd "H"
+    vim.cmd "vertical resize 42"
+end
+
 local function setup_formatters()
     vim.keymap.set("v", "<localleader>fi", function()
         return "c*<C-r>-*<Esc>"
@@ -35,5 +51,11 @@ local function setup_formatters()
         desc = "Wrap selected text with link ([]()), link valus is + register",
     })
 end
+
+vim.keymap.set("n", "gO", show_toc_side, {
+    buffer = true,
+    silent = true,
+    desc = "Show an Outline of the current buffer in a side pane",
+})
 
 setup_formatters()
