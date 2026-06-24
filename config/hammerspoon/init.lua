f1e34e8d1f0bdccf671b9ad3e0ffa08fbb9ecbb0
Ilya Ilyinykh <IlyasYOY@gmail.com>
2026-06-24 18:36:26 +0300

    chore(hammerspoon): remove unused Calendar and Mail app shortcuts
    Don't use Calendar and Mail through keyboard shortcuts, so remove their
    keybindings from the app launcher configuration.

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -2,9 +2,6 @@ for _, app in ipairs {
     { shortcut = "1", name = "WezTerm" },
     { shortcut = "2", name = "Zen" },
     { shortcut = "5", name = "Final Cut Pro" },
-
-    { shortcut = "7", name = "Calendar" },
-    { shortcut = "8", name = "Mail" },
 } do
     hs.hotkey.bind({ "alt" }, app.shortcut, function()
         hs.application.launchOrFocus(app.name)
