5313172ee962166f1dc984643a6186e0a2f73527
Ilya Ilyinykh <IlyasYOY@gmail.com>
2026-06-26 14:32:32 +0300

    feat(config/nvim): detect sqlfluff dialect from filename
    Set formatprg dialect to clickhouse or postgres when the filename
    contains those identifiers, falling back to ansi for everything else.
    This avoids manually switching dialects when editing SQL files for
    different engines.

--- config/nvim/after/ftplugin/sql.lua
+++ config/nvim/after/ftplugin/sql.lua
@@ -1,1 +1,10 @@
-vim.bo.formatprg = "sqlfluff format --dialect postgres -"
+local name = vim.fn.expand("%:t"):lower()
+
+local dialect = "ansi"
+if name:find "clickhouse" or name:find "ch" then
+    dialect = "clickhouse"
+elseif name:find "postgres" or name:find "pg" then
+    dialect = "postgres"
+end
+
+vim.bo.formatprg = string.format("sqlfluff format --dialect %s -", dialect)
